22c4db350c9c35d21ba050f3a510ee3ec0a563f3
Ilya <nedenilya@gmail.com>
2021-03-05 18:37:55 +0300

    Layout done

--- layout/assets/js/main.js
+++ layout/assets/js/main.js
@@ -1,15 +1,28 @@
-let sidebar_flag = true;
+$(document).ready(function(){
 
-$('.sidebar__button').on('click', function(){
-	if(sidebar_flag)
-	{
-		$('.sidebar__container').css('transform', 'translateX(0)');
-		$('.sidebar__arrow').css('transform', 'rotateY(180deg)');
-	}
-	else
-	{
-		$('.sidebar__container').css('transform', 'translateX(-300px)');
-		$('.sidebar__arrow').css('transform', 'rotateY(0deg)');
-	}
-	sidebar_flag = !sidebar_flag;
+	let left_sidebar_flag = true;
+	let right_sidebar_flag = true;
+
+	$('.sidebar__button').on('click', function(){
+		if(left_sidebar_flag){
+			$('.left__sidebar__container').css('transform', 'translateX(0)');
+			$('.sidebar__arrow').css('transform', 'rotateY(180deg)');
+		}
+		else{
+			$('.left__sidebar__container').css('transform', 'translateX(-300px)');
+			$('.sidebar__arrow').css('transform', 'rotateY(0deg)');
+		}
+		left_sidebar_flag = !left_sidebar_flag;
+	});
+
+	$('.history__button').on('click', function(){
+		if(right_sidebar_flag){
+			$('.right__sidebar__container').css('transform', 'translateX(0px)');
+		}
+		else{
+			$('.right__sidebar__container').css('transform', 'translateX(300px)');
+		}
+		right_sidebar_flag = !right_sidebar_flag;
+	});
+
 });
