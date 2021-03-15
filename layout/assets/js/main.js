964ebcefd41de4f3250fbe2a89fe54436e18b74e
Ilya <nedenilya@gmail.com>
2021-03-15 09:22:02 +0300

    responsive layout v2

--- layout/assets/js/main.js
+++ layout/assets/js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 
+
 	let left_sidebar_flag = false;
 	let right_sidebar_flag = true;
 	let action_popap = false;
+
+
 // left sidebar dlide
 	$('.sidebar__button').on('click', function(){
 		if(left_sidebar_flag){
@@ -19,23 +22,28 @@ $(document).ready(function(){
 // right sidebar slide
 	$('.history__button').on('click', function(){
 		if(right_sidebar_flag){
-			$('.right__sidebar__container').css('transform', 'translateX(0px)');
+			// $('.right__sidebar__container').css('transform', 'translateX(0px)');
+			$('.right__sidebar__container').css('display', 'flex');
 		}
 		else{
-			$('.right__sidebar__container').css('transform', 'translateX(300px)');
+			// $('.right__sidebar__container').css('transform', 'translateX(300px)');
+			$('.right__sidebar__container').css('display', 'none');
 		}
 		right_sidebar_flag = !right_sidebar_flag;
 	});
+
 
 // popap login frame
 	$('#signin').on('click', function(){
 		$('.login__popap').css('display', 'block');
 	});
 
+
 // popap register frame
 	$('#signup').on('click', function(){
 		$('.register__popap').css('display', 'block');
 	});
+
 
 // close frame button
 	$('.close__button').on('click', function(){
@@ -43,11 +51,13 @@ $(document).ready(function(){
 		$('.register__popap').css('display', 'none');
 	});
 
+
 	$('.item__act').on('click', function(e){
 		$(".action__popap", e.delegateTarget).toggleClass("active");
 		e.preventDefault();
 		action_popap = !action_popap;
 	});
+
 
 	$('.detail').on('click', function(){
 		alert('detail');
