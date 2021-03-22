88493f7f61204bc81c6bd9318e8f91d20ec1a295
Ilya <nedenilya@gmail.com>
2021-03-22 19:37:30 +0300

    added uploads entity

---
+++ src/Entity/Uploads.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UploadsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UploadsRepository::class)
+ */
+class Uploads
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=60, unique=false)
+     */
+    private $title; 
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=10, unique=false)
+     */
+    private $size;
+
+    public function setSize(string $size): self
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    public function getSize(): string
+    {
+        return $this->size;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=35, unique=false)
+     */
+    private $extension;
+
+    public function setExtension(string $extension): self
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    public function getExtension(): string
+    {
+        return $this->extension;
+    }
+
+    /**
+     * @ORM\Column(type="string", length=35, unique=false)
+     */
+    private DateTimeInterface $uploaded;
+
+    public function setUploaded(DateTimeInterface $uploaded): self
+    {
+        $this->uploaded = $uploaded;
+
+        return $this;
+    }
+    
+    public function getUploaded(): string
+    {
+        return $this->uploaded;
+    }
+}
