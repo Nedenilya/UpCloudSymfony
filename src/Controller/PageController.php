1147b296560212f1c0872c370ae0c816cdce307e
Ilya <nedenilya@gmail.com>
2021-03-22 20:56:22 +0300

    added uploads entity and migrations

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,17 @@ class PageController extends AbstractController
      */
     public function index(): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setUsername('Ilya')
+             ->setEmail('nedenilya@gmail.com')
+             ->setPassword('Ilya123')
+             ->setRoles(array('ROLE_USER'));
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+        
         return $this->render('main/index.html.twig', [
             'controller_name' => 'PageController',
         ]);
